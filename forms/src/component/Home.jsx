3301b6b9cc9bd604f6d4faf1ac042bc03f127976
MANJUPARKAVI T <manjutv1999@gmail.com>
2021-02-22 01:53:19 +0530

    Updated

---
+++ forms/src/component/Home.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import './Home.css';
+export default class Home extends Component {
+    
+
+    render() {
+        
+        return (
+            <div className="outline">
+                
+                <Link to="/"><button className="button1">Personal</button></Link>
+                <Link to="/buisness"><button className="button2">Buisness</button></Link>
+                <form className="form">
+                    <label >
+                        <input type="text" placeholder="FirstName" />
+                    </label>
+                    <br></br>
+                    <label>
+                        <input type="text" placeholder="LastName" />
+                    </label>
+                    <br/>
+                    <label >
+                        <input type="text" placeholder="Email" />
+                    </label>
+                    <br/>
+                    <label >
+                        <input type="text" placeholder="Mobile" />
+                    </label>
+
+                </form>
+                <Link to="/next"><button className="button3">Next</button></Link>
+            </div>
+        )
+    }
+}
